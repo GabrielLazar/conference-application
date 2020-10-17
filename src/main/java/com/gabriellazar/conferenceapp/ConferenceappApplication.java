fe3051c7d693e0ecc650d57698496b763b836e05
Gabriel <seal_sv@yahoo.com>
2020-10-17 16:08:13 -0500

    adding datasource tables and data

---
+++ src/main/java/com/gabriellazar/conferenceapp/ConferenceappApplication.java
@@ -0,0 +1,13 @@
+package com.gabriellazar.conferenceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabriellazar/conferenceapp/ConferenceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriellazar.conferenceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
