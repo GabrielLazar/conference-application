8e6feafb21f8b2f89548909fc573898eaddaabaf
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2020-12-05 19:30:47 -0600

    added delete operation

--- src/main/java/com/gabriellazar/conferenceapp/controllers/SpeakerController.java
+++ src/main/java/com/gabriellazar/conferenceapp/controllers/SpeakerController.java
@@ -4,6 +4,7 @@ import com.gabriellazar.conferenceapp.exceptions.DataNotFoundException;
 import com.gabriellazar.conferenceapp.models.ApiError;
 import com.gabriellazar.conferenceapp.models.Speaker;
 import com.gabriellazar.conferenceapp.repositories.SpeakerRepository;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,12 +22,14 @@ public class SpeakerController {
 
 
     @GetMapping
+    @ApiOperation(value = "Get all Speakers", notes = "Get all Speakers")
     public ResponseEntity<List<Speaker>> getSpeakers() {
         return ResponseEntity.status(HttpStatus.OK).body(speakerRepository.findAll());
     }
 
     @GetMapping("/{speakerId}")
-    public ResponseEntity<Speaker> getSpeakersById(@PathVariable final Long speakerId) {
+    @ApiOperation(notes = "Get a Speaker by Id", value = "Get a Speaker by Id")
+    public ResponseEntity<Speaker> getSpeakersById(@PathVariable(name = "speakerId") final Long speakerId) {
 
         Speaker speaker = speakerRepository.findById(speakerId).orElseThrow(
                 () -> new DataNotFoundException("Speaker not found with this id :: " + speakerId));
@@ -35,10 +38,18 @@ public class SpeakerController {
     }
 
     @PostMapping
+    @ApiOperation(value = "Create a new Speaker", notes = "Create a new Speaker")
     public ResponseEntity<Speaker> createSpeaker(@RequestBody @Valid Speaker speaker){
 
       Speaker speakerCreated =  speakerRepository.saveAndFlush(speaker);
       return ResponseEntity.status(HttpStatus.OK).body(speakerCreated);
+    }
+
+    @DeleteMapping("/{speakerId}")
+    @ApiOperation(value = "Delete a Speaker by Id", notes = "Delete a Speaker by Id")
+    public ResponseEntity<String> deleteSpeaker(@PathVariable(name = "speakerId") final Long speakerId){
+        speakerRepository.deleteById(speakerId);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("SUCCESS");
     }
 
 
