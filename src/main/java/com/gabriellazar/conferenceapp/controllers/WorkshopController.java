ab2c37ec22843035f8427addb667b3b4b6b1ec98
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-01-02 13:49:16 -0600

    create workshop and get workshop by id endpoint

--- src/main/java/com/gabriellazar/conferenceapp/controllers/WorkshopController.java
+++ src/main/java/com/gabriellazar/conferenceapp/controllers/WorkshopController.java
@@ -5,10 +5,9 @@ import com.gabriellazar.conferenceapp.services.WorkshopService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +25,18 @@ public class WorkshopController {
     @GetMapping("/")
     @ApiOperation(notes = "Get all workshops",value = "Get all workshops")
     public ResponseEntity<List<Workshop>> getAllWorkshop(){
-        return ResponseEntity.status(200).body(workshopService.getAllWorkshop());
+        return ResponseEntity.status(HttpStatus.OK).body(workshopService.getAllWorkshop());
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = "Get Workshop by Id", notes = "Get Workshop by Id")
+    public ResponseEntity<Workshop> getWorkshopById(@PathVariable(name = "id") final Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(workshopService.getWorkshopById(id));
+    }
+
+    @PostMapping
+    @ApiOperation(value = "Create a Workshop", notes = "Create a Workshop")
+    public ResponseEntity<Workshop> createWorkshop(@RequestBody final Workshop workshop){
+        return ResponseEntity.status(HttpStatus.CREATED).body(workshopService.createWorkshop(workshop));
     }
 }
