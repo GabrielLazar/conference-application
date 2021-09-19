2d9a186d634f6e4e22e5c5081ed85b091139496d
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-09-18 22:17:56 -0500

    adding logs and exception handling to speaker service

--- src/main/java/com/gabriellazar/conferenceapp/services/impl/SpeakerServiceImpl.java
+++ src/main/java/com/gabriellazar/conferenceapp/services/impl/SpeakerServiceImpl.java
@@ -4,12 +4,16 @@ import com.gabriellazar.conferenceapp.exceptions.DataNotFoundException;
 import com.gabriellazar.conferenceapp.models.Speaker;
 import com.gabriellazar.conferenceapp.repositories.SpeakerRepository;
 import com.gabriellazar.conferenceapp.services.SpeakerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 import java.util.List;
 
 @Service
+@Slf4j
 public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository speakerRepository;
@@ -21,36 +25,57 @@ public class SpeakerServiceImpl implements SpeakerService {
 
     @Override
     public List<Speaker> getAllSpeakers() {
-        return speakerRepository.findAll();
+        List<Speaker> speakers = null;
+        try {
+            speakers = speakerRepository.findAll();
+            log.info("Successfully getting all speakers :: {}", speakers);
+        } catch (Exception e) {
+            log.error("Exception in getting all speakers :: Exception {}", e);
+            return Collections.emptyList();
+        }
+        return speakers;
     }
 
     @Override
     public Speaker getSpeakerById(Long id) {
         return speakerRepository.findById(id)
-                .orElseThrow(() -> new DataNotFoundException("Speaker not found with this id :: " +  id));
+                .orElseThrow(() -> new DataNotFoundException("Speaker not found with this id :: " + id));
     }
 
     @Override
     public Speaker createSpeaker(Speaker speaker) {
-
-     return speakerRepository.saveAndFlush(speaker);
+        Speaker savedSpeaker = null;
+        try {
+            savedSpeaker = speakerRepository.saveAndFlush(speaker);
+            log.info("Successfully saved speaker :: {}", savedSpeaker);
+        } catch (Exception e) {
+            log.error("Exception in saving speaker :: {}, Exception :: {}", speaker, e);
+        }
+        return getSpeakerById(savedSpeaker.getSpeaker_id());
     }
 
     @Override
-    public Speaker editSpeakerById(Long id,Speaker speaker) {
-
-        Speaker existingSpeaker = getSpeakerById(id);
-        BeanUtils.copyProperties(speaker, existingSpeaker,"speaker_id");
-        return speakerRepository.saveAndFlush(existingSpeaker);
+    public Speaker editSpeakerById(Long id, Speaker speaker) {
+        try {
+            Speaker existingSpeaker = getSpeakerById(id);
+            BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+            speakerRepository.saveAndFlush(existingSpeaker);
+            log.info("Updating speaker with id :: {} Speaker :: {}", id, speaker);
+        } catch (Exception e) {
+            log.error("Exception in updating speaker id :: {} Speaker :: {} Exception", id, speaker, e);
+        }
+        return getSpeakerById(id);
     }
 
     @Override
     public void deleteSpeakerById(Long id) {
-        speakerRepository.deleteById(id);
+        try {
+            speakerRepository.deleteById(id);
+            log.info("Successfully deleted speaker with id :: {}", id);
+        } catch (Exception e) {
+            log.error("Exception in deleting speaker with id :: {}", id);
+        }
+
     }
 
-    @Override
-    public List<Speaker> getSpeakerByCompany(String company) {
-        return speakerRepository.findAllByCompany(company);
-    }
 }
