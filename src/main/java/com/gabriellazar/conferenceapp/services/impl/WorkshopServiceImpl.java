7fe80a110403684cd35ea00cee4aa08984713b57
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-09-28 21:40:03 -0500

    adding logs to workshop service

--- src/main/java/com/gabriellazar/conferenceapp/services/impl/WorkshopServiceImpl.java
+++ src/main/java/com/gabriellazar/conferenceapp/services/impl/WorkshopServiceImpl.java
@@ -4,13 +4,16 @@ import com.gabriellazar.conferenceapp.exceptions.DataNotFoundException;
 import com.gabriellazar.conferenceapp.models.Workshop;
 import com.gabriellazar.conferenceapp.repositories.WorkshopRepository;
 import com.gabriellazar.conferenceapp.services.WorkshopService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
+@Slf4j
 public class WorkshopServiceImpl implements WorkshopService {
 
     private WorkshopRepository workshopRepository;
@@ -22,7 +25,15 @@ public class WorkshopServiceImpl implements WorkshopService {
 
     @Override
     public List<Workshop> getAllWorkshop() {
-        return workshopRepository.findAll();
+        List<Workshop> workshops = null;
+        try {
+            workshops = workshopRepository.findAll();
+            log.info("Getting all workshops :: {}", workshops);
+        } catch (Exception e) {
+            log.error("Exception is getting all sessions :: {}", e);
+            return Collections.emptyList();
+        }
+        return workshops;
     }
 
     @Override
@@ -33,23 +44,42 @@ public class WorkshopServiceImpl implements WorkshopService {
 
     @Override
     public Workshop createWorkshop(Workshop workshop) {
-        return workshopRepository.saveAndFlush(workshop);
+        Workshop savedWorkshop = null;
+        try {
+            savedWorkshop = workshopRepository.saveAndFlush(workshop);
+            log.info("Successfully saving workshop :: {}", savedWorkshop);
+        } catch (Exception e) {
+            log.error("Exception in saving workshop :: {} Exception ::{}", workshop, e);
+        }
+        return getWorkshopById(savedWorkshop.getWorkshop_id());
     }
 
     @Override
     public Workshop editWorkshopById(Long id, Workshop workshop) {
-        Workshop existingWorkshop = getWorkshopById(id);
-        BeanUtils.copyProperties(workshop, existingWorkshop, "workshop_id");
-        return workshopRepository.saveAndFlush(existingWorkshop);
+        try {
+            Workshop existingWorkshop = getWorkshopById(id);
+            BeanUtils.copyProperties(workshop, existingWorkshop, "workshop_id");
+            Workshop savedWorkshop = workshopRepository.saveAndFlush(existingWorkshop);
+            log.info("Updating workshop with id ::{} Workshop :: {}", id, savedWorkshop);
+        } catch (Exception e) {
+            log.error("Exception in updating workshop id ::{}, workshop ::{}, Exception ::{}", id, workshop, e);
+        }
+        return getWorkshopById(id);
     }
 
     @Override
     public void deleteWorkshopById(Long id) {
-        workshopRepository.deleteById(id);
+        try {
+            workshopRepository.deleteById(id);
+            log.info("Successfully deleted Workshop with id ::{}", id);
+        } catch (Exception e) {
+            log.error("Exception in deleting Workshop with id :: {}", id);
+        }
+
     }
 
     @Override
     public List<Workshop> getWorkshopByRoom(String room) {
-      return workshopRepository.findByRoom(room);
+        return workshopRepository.findByRoom(room);
     }
 }
