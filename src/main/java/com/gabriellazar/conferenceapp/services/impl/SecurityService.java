2ea03e89852d03db3413bf4c88c8c52f6a570dc2
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-10-22 00:27:19 -0500

    Security Service

---
+++ src/main/java/com/gabriellazar/conferenceapp/services/impl/SecurityService.java
@@ -0,0 +1,41 @@
+package com.gabriellazar.conferenceapp.services.impl;
+
+import com.gabriellazar.conferenceapp.exceptions.DataNotFoundException;
+import com.gabriellazar.conferenceapp.models.Attendee;
+import com.gabriellazar.conferenceapp.services.AttendeeService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecurityService implements UserDetailsService {
+
+    private AttendeeService attendeeService;
+
+    public SecurityService(AttendeeService attendeeService) {
+        this.attendeeService = attendeeService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Attendee attendee = attendeeService.getAttendeeByEmail(email);
+        if(attendee == null){
+            throw new DataNotFoundException("Attendee not found with email :: " + email);
+        }
+
+        return new org.springframework.security.core.userdetails.User(attendee.getEmail(),attendee.getPassword(),getAuthority(attendee);
+
+    }
+
+    public List<GrantedAuthority> getAuthority(Attendee attendee ){
+
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority(attendee.getRole()));
+        return grantedAuthorities;
+    }
+}
