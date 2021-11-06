bbb960b6609a091bf21d34e4d0cd1bffa057b620
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-11-06 16:41:14 -0500

    adding roles to the speaker controller

--- src/main/java/com/gabriellazar/conferenceapp/controllers/SpeakerController.java
+++ src/main/java/com/gabriellazar/conferenceapp/controllers/SpeakerController.java
@@ -39,6 +39,7 @@ public class SpeakerController {
 
     @GetMapping("/{speakerId}")
     @ApiOperation(notes = "Get a Speaker by Id", value = "Get a Speaker by Id")
+    @PreAuthorize("hasAnyAuthority('ADMIN','ATTENDEE')")
     public ResponseEntity<Speaker> getSpeakersById(@PathVariable(name = "speakerId") final Long speakerId) {
 
         return ResponseEntity.status(HttpStatus.OK).body(speakerService.getSpeakerById(speakerId));
@@ -46,6 +47,7 @@ public class SpeakerController {
 
     @PostMapping
     @ApiOperation(value = "Create a new Speaker", notes = "Create a new Speaker")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public ResponseEntity<Speaker> createSpeaker(@RequestBody @Valid final Speaker speaker) {
 
         Speaker speakerCreated = speakerService.createSpeaker(speaker);
@@ -54,6 +56,7 @@ public class SpeakerController {
 
     @DeleteMapping("/{speakerId}")
     @ApiOperation(value = "Delete a Speaker by Id", notes = "Delete a Speaker by Id")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public ResponseEntity<String> deleteSpeaker(@PathVariable(name = "speakerId") final Long speakerId) {
         speakerService.deleteSpeakerById(speakerId);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body("SUCCESS");
@@ -61,6 +64,7 @@ public class SpeakerController {
 
     @PutMapping("/{speakerId}")
     @ApiOperation(value = "Update an existing Speaker", notes = "Update an existing Speaker")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public ResponseEntity<Speaker> updateSpeaker(
             @PathVariable(name = "speakerId") final Long id,
             @RequestBody @Valid @NotNull final Speaker speaker) {
@@ -68,12 +72,5 @@ public class SpeakerController {
         Speaker existingSpeaker = speakerService.editSpeakerById(id, speaker);
         return ResponseEntity.status(HttpStatus.OK).body(existingSpeaker);
     }
-
-//    @GetMapping("/company/{companyName}")
-//    @ApiOperation(value = "Get all the speakers by company", notes = "Get all the speakers by company")
-//    public ResponseEntity<List<Speaker>> getSpeakersByCompany(@PathVariable(name = "companyName") final String companyName) {
-//        return ResponseEntity.status(HttpStatus.OK).body(speakerService.getSpeakerByCompany(companyName));
-//    }
-
 
 }
