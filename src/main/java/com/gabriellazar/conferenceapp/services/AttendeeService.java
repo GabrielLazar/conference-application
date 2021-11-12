ecef7a5d7893065f2d653d9f17715af0bf17c418
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-11-12 08:59:21 -0600

    save attendee service

--- src/main/java/com/gabriellazar/conferenceapp/services/AttendeeService.java
+++ src/main/java/com/gabriellazar/conferenceapp/services/AttendeeService.java
@@ -5,4 +5,5 @@ import com.gabriellazar.conferenceapp.models.Attendee;
 public interface AttendeeService {
 
     Attendee getAttendeeByEmail(String email);
+    Attendee saveAttendee(Attendee attendee);
 }
