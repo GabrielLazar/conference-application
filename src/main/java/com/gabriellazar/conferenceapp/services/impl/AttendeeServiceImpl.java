4b7e3c58b4b7052597ab2092e1590dd16d026d58
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-11-13 15:43:28 -0600

    saving attendee

--- src/main/java/com/gabriellazar/conferenceapp/services/impl/AttendeeServiceImpl.java
+++ src/main/java/com/gabriellazar/conferenceapp/services/impl/AttendeeServiceImpl.java
@@ -4,9 +4,11 @@ package com.gabriellazar.conferenceapp.services.impl;
 import com.gabriellazar.conferenceapp.models.Attendee;
 import com.gabriellazar.conferenceapp.repositories.AttendeeRepository;
 import com.gabriellazar.conferenceapp.services.AttendeeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class AttendeeServiceImpl implements AttendeeService {
 
     private AttendeeRepository attendeeRepository;
@@ -22,6 +24,13 @@ public class AttendeeServiceImpl implements AttendeeService {
 
     @Override
     public Attendee saveAttendee(Attendee attendee) {
-        return attendeeRepository.save(attendee);
+        Attendee savedAttendee = null;
+        try{
+            savedAttendee = attendeeRepository.save(attendee);
+            log.info("Successfully saved attendee :: {}", savedAttendee);
+        } catch (Exception e){
+            log.error("Exception in saving attendee :: {}, Exception :: {}", attendee, e);
+        }
+        return getAttendeeByEmail(savedAttendee.getEmail());
     }
 }
